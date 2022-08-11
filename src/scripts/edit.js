00ac8a8acd5ab1027820055639b1becf7fe9f5da
Wouter <woutervandervelde2000@hotmail.com>
2022-08-11 22:00:29 +0200

    started with the breadcrumb layout

--- src/scripts/edit.js
+++ src/scripts/edit.js
@@ -1,1 +1,15 @@
 const revertible = JSON.parse(localStorage.getItem("conversion_data"));
+
+const pages = ['title', 'description', 'ingredients', 'steps'];
+
+const editNavigate = (page) => {
+    setNavigationActive(page);
+}
+
+const setNavigationActive = (page) => {
+    const navigations = document.querySelector(".breadcrumbs-container").querySelectorAll("a");
+    navigations.forEach(elem => elem.classList.remove("active"));
+
+    const navigation = document.querySelector(".breadcrumbs-container").querySelector(`.${page}`);
+    navigation.classList.add("active");
+}
