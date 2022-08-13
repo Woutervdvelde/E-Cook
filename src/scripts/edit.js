886246bac2ab2d32d6962f53bc5298f5e30a8504
Wouter <woutervandervelde2000@hotmail.com>
2022-08-13 21:07:16 +0200

    page switching now with template content

--- src/scripts/edit.js
+++ src/scripts/edit.js
@@ -1,6 +1,8 @@
 const revertible = JSON.parse(localStorage.getItem("conversion_data"));
+const editContainer = document.getElementById("edit_container");
 const nextButton = document.getElementById("edit_next_button");
 const backButton = document.getElementById("edit_back_button");
+const finishButton = document.getElementById("edit_finish_button");
 
 const pages = ['title', 'description', 'ingredients', 'steps'];
 let currentPage = pages[0];
@@ -15,6 +17,7 @@ const editNavigate = (page) => {
     if (page) {
         currentPage = page;
         setNavigationActive(page);
+        setNavigatedTemplate(page);
     }
     checkButtonToggle();
 }
@@ -26,12 +29,18 @@ const checkButtonToggle = () => {
     if (!pages[pages.indexOf(currentPage) - 1])
         disableBackButton();
     else enableBackButton();
+
+    if (currentPage == pages[pages.length - 1])
+        enableFinishButton();
+    else disableFinishButton();
 }
 
 const enableNextButton = () => { nextButton.style.opacity = 1; nextButton.disabled = false };
 const disableNextButton = () => { nextButton.style.opacity = 0; nextButton.disabled = true };
 const enableBackButton = () => { backButton.style.opacity = 1; backButton.disabled = false };
 const disableBackButton = () => { backButton.style.opacity = 0; backButton.disabled = true };
+const enableFinishButton = () => { finishButton.style.opacity = 1; finishButton.disabled = false };
+const disableFinishButton = () => { finishButton.style.opacity = 0; finishButton.disabled = true };
 
 
 const setNavigationActive = (page) => {
@@ -40,6 +49,14 @@ const setNavigationActive = (page) => {
 
     const navigation = document.querySelector(".breadcrumbs-container").querySelector(`.${page}`);
     navigation.classList.add("active");
+}
+
+const setNavigatedTemplate = (page) => {
+    const template = document.getElementById(`edit_template_${page}`);
+    if (!template) return;
+
+    editContainer.innerHTML = '';
+    editContainer.appendChild(template.content.cloneNode(true));
 }
 
 window.onload = () => {
