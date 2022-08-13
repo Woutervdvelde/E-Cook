87ebe3cbf0a0601f80478d74d68042aef80a748f
Wouter <woutervandervelde2000@hotmail.com>
2022-08-13 20:36:47 +0200

    edit buttons and switching current page

--- src/scripts/edit.js
+++ src/scripts/edit.js
@@ -1,10 +1,38 @@
 const revertible = JSON.parse(localStorage.getItem("conversion_data"));
+const nextButton = document.getElementById("edit_next_button");
+const backButton = document.getElementById("edit_back_button");
 
 const pages = ['title', 'description', 'ingredients', 'steps'];
+let currentPage = pages[0];
 
 const editNavigate = (page) => {
-    setNavigationActive(page);
+    if (page == 'next')
+        page = pages[pages.indexOf(currentPage) + 1];
+
+    if (page == 'back')
+        page = pages[pages.indexOf(currentPage) - 1];
+
+    if (page) {
+        currentPage = page;
+        setNavigationActive(page);
+    }
+    checkButtonToggle();
 }
+
+const checkButtonToggle = () => {
+    if (!pages[pages.indexOf(currentPage) + 1])
+        disableNextButton();
+    else enableNextButton();
+    if (!pages[pages.indexOf(currentPage) - 1])
+        disableBackButton();
+    else enableBackButton();
+}
+
+const enableNextButton = () => { nextButton.style.opacity = 1; nextButton.disabled = false };
+const disableNextButton = () => { nextButton.style.opacity = 0; nextButton.disabled = true };
+const enableBackButton = () => { backButton.style.opacity = 1; backButton.disabled = false };
+const disableBackButton = () => { backButton.style.opacity = 0; backButton.disabled = true };
+
 
 const setNavigationActive = (page) => {
     const navigations = document.querySelector(".breadcrumbs-container").querySelectorAll("a");
@@ -12,4 +40,8 @@ const setNavigationActive = (page) => {
 
     const navigation = document.querySelector(".breadcrumbs-container").querySelector(`.${page}`);
     navigation.classList.add("active");
+}
+
+window.onload = () => {
+    checkButtonToggle();
 }
